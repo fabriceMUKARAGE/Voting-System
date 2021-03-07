1659899be10d87bd22ff5a8fd0593050bd5447d1
fabriceMUKARAGE <79330798+fabriceMUKARAGE@users.noreply.github.com>
2021-03-07 16:33:10 +0100

    Update VotingSystem.java

--- VotingSystem.java
+++ VotingSystem.java
@@ -151,18 +151,18 @@ public class DistrictElectoralOffice extends PullingStation{
 
 public class VotingSystem {
 	public static void main(String[] arg) {
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter the number of candidate: ");
-	int number=input.nextInt(); 
-	for(int i=0;i<number;i++){
-		System.out.println("Enter the name of candidate: ");
-		int name=input.nextLine(); 
-		System.out.println("Enter the candidate number: ");
-		int num=input.nextInt(); 
-		System.out.println("Enter the politcal party: ");
-		int party=input.nextLine(); 
-	
-		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number of candidate: ");
+		int number=input.nextInt(); 
+		for(int i=0;i<number;i++){
+			System.out.println("Enter the name of candidate: ");
+			int name=input.nextLine();
+			System.out.println("Enter the candidate number: ");
+			int num=input.nextInt(); 
+			System.out.println("Enter the politcal party: ");
+			int party=input.nextLine(); 
+			Candidate candidate = new Candidate(name, num, party);
+		}
 	}
 }
 
